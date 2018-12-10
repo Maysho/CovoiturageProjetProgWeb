bd2718481fc3ab4d67647fd3d129b4119be29828
antoinetb123 <45195714+antoinetb123@users.noreply.github.com>
2018-12-10 22:10:18 +0100

    MDPOublie

--- modules/mod_connexion/cont_connexion.php
+++ modules/mod_connexion/cont_connexion.php
@@ -46,13 +46,61 @@ class cont_connexion
 	}
 	public function ChercheMotDePasseOublier()
 	{
-		if (isset($_POST["email"]) {
-			$this->modele->verifieMail($_POST["email"]);
-			$this->modele->envoieMailMdp();
+		if (isset($_GET["email"])&& $this->modele->verifieMail($_GET["email"])) {
+			$this->modele->envoieMailMdp($_GET["email"]);
+			self::affichePageToken(0,$_GET["email"]);
 		}
+		else
+			self::AfficheMotDePasseOublier(1);
+		
 		
 	}
-
+	public function VerifieToken()
+	{
+		if (empty($_GET["email"])) {
+			self::AfficheMotDePasseOublier(1);
+		}
+		elseif (isset($_POST['token'])  && $this->modele->verifieToken($_GET["email"],$_POST["token"])) {
+			header("Location: index.php?module=mod_connexion&action=ChangementMDP&email=".$_GET['email']);
+		}
+		else
+			self::affichePageToken(1,$_GET["email"]);
+	}
+	public function affichePageToken($value,$email)
+	{
+		$this->vue->pageToken($value,$email);
+	}
+	public function ChangementMDP()
+	{
+		if (empty($_GET["email"])) {
+			self::AfficheMotDePasseOublier(1);
+		}
+		else{
+			$this->vue->affichePageChangementMPD(0,$_GET["email"]);
+		
+		}
+	}
+	public function VerifieMPD()
+	{
+		if (isset($_POST['mdp'])&&isset($_POST['mdpconf']) && $this->modele->verifieMDP($_POST['mdp'],$_POST['mdpconf'])) {
+			$_SESSION['id']=$this->modele->recupereID($_GET['email']);
+			echo $_SESSION['id'];
+			if($_SESSION['id']<0){
+				unset($_SESSION['id']);
+				$this->vue->pageConnexion(1);
+			}
+			else{
+				header("Location: index.php");
+			}
+		}
+		else
+			$this->vue->affichePageChangementMPD(1,$_GET["email"]);
+	}
+	public function deconnexion($value='')
+	{
+		unset($_SESSION['id']);
+		header("Location: index.php");
+	}
 	public function AfficheMotDePasseOublier($val)
 	{
 		$this->vue->motDePasseOublier($val);
