4eceac7f2498edb8a20a7ed5c254a70a870511de
BastianPadiglione <45195612+BastianPadiglione@users.noreply.github.com>
2018-12-17 20:44:12 +0100

    Revert "Bastian"

--- scriptphp/formTrajet.php
+++ scriptphp/formTrajet.php
@@ -1,4 +1,5 @@
 <?php  
+session_start();
 include_once __DIR__.'/../modules/mod_trajet/modele_trajet.php';
 $modele_trajet=new modele_trajet();
 
@@ -6,6 +7,12 @@ $modele_trajet=new modele_trajet();
 
 if( isset($_POST['soustrajet']) && isset($_POST['descriptionTrajet']) && isset($_POST['placeTotale'])){
 	if($modele_trajet->verifCreationTrajet3($_POST['soustrajet'], $_POST['descriptionTrajet'],$_POST['placeTotale'])){
+		echo "ok";
+	}
+}
+
+if( isset($_POST['immatriculation']) && isset($_POST['critair']) && isset($_POST['hybride'])){
+	if($modele_trajet->ajoutVehicule($_POST['immatriculation'],$_POST['critair'],$_POST['hybride'])){
 		echo "ok";
 	}
 }
