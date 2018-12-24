07dae198d2c1f536c913fba7c0284ad826a58740
antoinetb123 <45195714+antoinetb123@users.noreply.github.com>
2018-12-24 17:25:06 +0100

    rajout de la partit de william

--- modules/mod_trajet/vue_trajet.php
+++ modules/mod_trajet/vue_trajet.php
@@ -47,17 +47,191 @@ class vue_Trajet extends VueGenerique{
 									<img class="col-md-12 img-thumbnail" alt="Map de la France" src="photos/Black.png">
 								</div>
 							</div>
-						</div > 
-						<hr>
-		  			</div>
-		  			<div class="row">
-					    <label><input type="checkbox" name="notificationJoin">Me prévenir lorsqu'un passager s'inscrit au trajet</label>
-					</div>
-		  			<button id="envoiTrajet" class="btn btn-primary">C'est parti! <i class="fas fa-car-side"></i></button>
-	  		</section>
-	  	</div>
+							<hr>
+						</div>
+						<div>
+							<div class="text-left">
+								<h2>Dates et Horaires</h2>
+				  					<!-- <div class="row container" >
+				  						<h2>Dates et Horaires</h2>
+				  						<label>Aller-Retour<input type="checkbox" name="allerRetour"></label>
+				  					</div> --> 
+				  				</div>
+				  				<div class="text-left form-group row" >
+				  					<div class="col-md-4 row form-group text-center">
+				  						<label class="col-md-5"><i style=" color: green;"class="fab fa-font-awesome-flag"></i> Date de l'aller </label>
+				  						<input class="offset-md-1 col-md-5 form-control" type="date" id="dateDepart" value="<?php echo date('Y-m-d') ?>">
+				  					</div>
 
-		<?php
+				  					<div class="col-md-3 row form-group text-center">
+				  						<label class="col-md-4">Heure <i class="far fa-clock"></i></label>
+				  						<input class="offset-md-2 col-md-4 form-control" type="time" id="heureDepart" value="<?php echo date('h:i') ?>">
+				  					</div>
+				  				</div>
+				  				
+				  				<div class="text-left form-group" id="checkpoint" hidden>
+				  					<div class="form-group row checkpoint" id="checkpoint0" >
+				  						<div class="col-md-4 row form-group text-center">
+				  							<label class="col-md-5"><i class="fab fa-font-awesome-flag"></i> Date Etape </label>
+				  							<input type="date" class="offset-md-1 col-md-5 form-control" id="date0" value="<?php echo date('Y-m-d') ?>">
+				  						</div>
+
+				  						<div class="col-md-3 row form-group text-center">
+				  							<label class="col-md-4">Heure <i class="far fa-clock"></i></label>
+				  							<input class="offset-md-2 col-md-4 form-control" type="time" id="heure0" value="<?php echo date('h:i') ?>">
+				  						</div>
+
+				  						<div class="col-md-4 row form-group text-center">
+				  							<label class="col-md-4">Prix</label>
+				  							<input class="offset-md-1 col-md-2 text-right form-control" id="prix0" value="0">
+				  						</div>
+				  					</div>
+				  				</div> 
+
+				  				<div class="form-group text-left row">
+				  					<div class="col-md-4 row form-group text-center">
+				  						<label class="col-md-5"><i style=" color: red;" class="fab fa-font-awesome-flag"></i> Date Arrivee </label>
+				  						<input type="date" class="offset-md-1 col-md-5 form-control" id="dateArrivee" value="<?php echo date('Y-m-d') ?>">
+				  					</div>
+
+				  					<div class="col-md-3 row form-group text-center">
+				  						<label class="col-md-4" >Heure <i class="far fa-clock"></i></label>
+				  						<input class="offset-md-2 col-md-4 form-control" type="time" id="heureArrivee" value="<?php echo date('h:i') ?>">
+				  					</div>
+
+				  					<div class="col-md-4 row form-group text-center">
+				  						<label class="col-md-4">Prix</label>
+				  						<input class="offset-md-1 col-md-2 text-right form-control" id="prixArrivee" value="0">
+				  					</div>
+				  				</div> 
+
+				  				<!-- <div class="text-left form-group" hidden>
+				  					<label>Date du retour</label>
+				  						<input type="date" name="" value="">
+				  					<label>Heure</label>
+										<input class="col-md-2" type="time" name="soustrajet[][heure]" value="<?php echo date('h:i') ?>">
+				  					<label>prix</label>
+				  						<input class="col-md-1" name="prix" value="0"	>
+				  					</div> -->
+				  					<hr>
+				  				</div>
+				  				<div> 
+				  					<h2 class="text-left"> Autres Modalités	</h2>
+				  					<div class="row">
+				  						<div class="col-md-6"> 
+				  							<div class="text-left">
+				  								<div class="row">
+				  									<label class="col-md-4" for="idVehicule">Mon Vehicule</label>
+				  									<select class="offset-md-1 col-md-6 text-center form-control" id="idVehicule">
+				  										<?php if(empty($listeVehicule)) {
+				  										?>
+			  											<option value="">--Please choose an option--</option>
+				  										<?php
+				  										}
+				  										 ?>
+				  										<?php 
+				  										foreach ($listeVehicule as $key => $value) {
+				  											?>
+			  											<option <?php if($key == 0) echo "selected";?> class="bidule" data-url="<?php echo $value['urlPhoto']?>" value="<?php echo $key ?>"><?php echo $value['immatriculation'] ?> </option>	
+				  											<?php
+				  										}
+				  										?>
+				  									</select>
+				  								</div>
+
+				  								<div class="row">
+				  									<!-- Trigger the modal with a button -->
+				  									<button type="button" class="offset-md-5 col-md-6  btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Ajouter un Vehicule <i class="fas fa-car"></i></button>
+
+				  									<!-- Modal -->
+				  									<div class="modal fade" id="myModal" role="dialog">
+				  										<div class="modal-dialog modal-lg">
+
+				  											<!-- Modal content-->
+				  											<div class="modal-content">
+				  												<div class="modal-header">
+				  													<button type="button" class="close" data-dismiss="modal">&times;</button>
+				  												</div>
+
+				  												<div class="modal-body">
+				  													<h3 class="modal-title text-center">Votre nouveau Vehicule</h3>
+				  													<div class="row form-group offset-md-1">
+				  														<label class="col-md-5" for="immatriculation">Immatriculation</label>
+				  														<input class="col-md-5 form-control" type="text" id="immatriculation" placeholder="AA-000-AA">
+				  													</div>
+				  													<div class="row offset-md-1">
+				  														<label class="col-md-5" for="critair">Crit'air</label>
+				  														<select class="col-md-5 form-control" id="critair">
+				  															<option value="0">Vert</option>
+				  															<option value="1">1</option>
+				  															<option value="2">2</option>
+				  															<option value="3">3</option>
+				  															<option value="4">4</option>
+				  															<option value="5">5</option>
+				  															<option value="6">Je n'ai pas certifié mon véhicule</option>
+				  														</select>
+				  													</div>
+
+				  													<div class="row offset-md-1">
+				  														<label class="col-md-2" for="hybride">Hybride</label>	
+				  														<div class="row offset-md-5">
+				  															<input type="checkbox" id="hybride" name="hybride">
+				  														</div>
+				  													</div>
+
+				  													<div class="row offset-md-1">
+				  														<label class="col-md-5" for="photoVehicule">Photo du Vehicule</label>
+				  														<img id="defaultThumb"class="col-md-5 thumb img-fluid" src="photos/Black.png" alt="Photo du vehicule">
+				  														<input type="hidden" id="photoVehicule" name="MAX_FILE_SIZE" value="5000000" />
+				  														<input class="offset-md-5" type="file" id="photoCar"name="photoCar">
+
+				  													</div>
+
+				  													<div class="text-center">
+				  														<button id="addCar" data-dismiss="modal" class="btn btn-primary"> Ajouter </button>
+				  													</div>
+				  												</div>
+
+				  												<div class="modal-footer">
+				  													<button class="btn btn-default" data-dismiss="modal">Close</button>
+				  												</div>
+				  											</div>
+
+				  										</div>
+				  									</div>									  
+				  								</div>
+
+
+				  								<div class="row">
+				  									<label class="col-md-4" for="placeTotale">Nombre de place</label>
+				  									<input class="offset-md-1 col-md-6 text-right form-control" type="text"  name="placeTotale"   id="placeTotale"  value="0">
+				  								</div>
+				  							</div >
+				  						</div>
+				  						<div class="col-md-6"> 
+				  							<img class="col-md-12 img-thumbnail img-fluid" id="imgCar"alt="Voiture" src="photos/Black.png">
+				  							<!-- <?php if(isset($listeVehicule[$key]['urlPhoto'])) echo $listeVehicule[$key]['urlPhoto']; else echo "/photos/Black.png"; ?> -->
+				  						</div>
+				  					</div>
+				  					<div class="row">
+				  						<div class="col-md-12">
+				  							<div class="text-left form-group">
+				  								<label for="descriptionTrajet">Detail du voyage</label>
+				  								<textarea class="form-control" name="descriptionTrajet" id="descriptionTrajet" rows="4"  placeholder="Description du trajet"></textarea>
+				  							</div>
+				  						</div>
+				  					</div > 
+				  					<hr>
+				  				</div>
+				  				<div class="container row">
+				  					<label><input type="checkbox" name="notificationJoin">Me prévenir lorsqu'un passager s'inscrit au trajet</label>
+				  				</div>
+				  				<button id="envoiTrajet" class="btn btn-primary">C'est parti! <i class="fas fa-car-side"></i></button>
+				  			</section>
+				  		</div>
+
+				  		<?php
+				  	
 	}
 	public function afficheTrajet($value,$infoTrajet,$user,$idS,$tabSt,$tabCom)
 	{
@@ -160,9 +334,9 @@ class vue_Trajet extends VueGenerique{
 
 		</div>
 		<?php
-		var_dump($user);
+		/*var_dump($user);
 		var_dump(array_column($user, 'utilisateur_idutilisateur'));
-		var_dump(array_column($user, 'sousTrajet_idsousTrajet'));
+		var_dump(array_column($user, 'sousTrajet_idsousTrajet'));*/
 		$idUtilisateur=array_column($user, 'utilisateur_idutilisateur');
 		$idSousTrajets=array_column($user, 'sousTrajet_idsousTrajet');
 			if ($value==1) {
@@ -372,19 +546,23 @@ class vue_Trajet extends VueGenerique{
 				</div>
 <?php	   				
 	   			}?>
-				<div class="row col-12">
+				<form method="POST" action="" id="formCommentairePageTrajet" class="row col-12">
 
 				<div class="col-3 col-md-2 offset-md-1">
 					<img src="home.jpg" class="img-fluid">
 				</div>
 				
 				<div class="col-7 col-md-9">
-					<textarea type="textarea" class="col" form="" name="commentaire" style="resize: none;"> </textarea>
+					<textarea type="textarea" class="col" form="formCommentairePageTrajet" name="commentaire" style="resize: none;"> </textarea>
+				</div>
+				<div class="col-3 col-md-2 offset-md-1">
+					<label  for="note">note:</label>
+					<input class ="col-12  col-md-6" type="text" id="note"name="note">
 				</div>	
 				<div class="col-md-3 col-6 offset-md-9 offset-7">
 					<input type="submit" class="col" name="">
 				</div>				
-				</div>
+				</form>
 				</div>
 
 
