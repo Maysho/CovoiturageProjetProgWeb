7a7b2ee5993664d47ad7f3561b3c49262f6e476b
William <wlin@iut.univ-paris8.fr>
2019-01-06 21:43:14 +0100

    Revert "Ajout vehicule affichage"
    This reverts commit 64ce846bdb4b3ab2ccac8d9e7d0c39389150b179.

--- jquery.js
+++ jquery.js
@@ -223,15 +223,23 @@ $('#addCar').on('click', function(e){
     type: 'POST', // For jQuery < 1.9
     data:formData,
     success : function(txt){
-      
+      // console.log(txt);
       $('#immatriculation').val("");
       $('#critair').val("0");
       $('#hybride').prop('checked', false);
       $('#defaultThumb').attr('src', 'photos/Black.png');
       $('#photoCar').val("");
-      
-      $(document).find("#immatriculationCar").last().append("<option class='voitureSelection' value="+immatriculation +">"+ immatriculation +"</option>");
-      $(document).find("#imgCar").attr("src",txt);
+      // $(document).find('.voitureSelection').last().attr("value");
+      // var voiture= $(document).find('.voitureSelection').last();
+      // if(voiture.attr("value") == -1){
+      //   var ajout = voiture.clone();
+      //   ajout.attr('data-url', '');
+      //   ajout.attr('value', '0');
+      //   ajout.attr('selected', '');
+      //   ajout.html("nouvelle voiture");
+      //   $(document).find('.voitureSelection').append(ajout);
+      // }
+
 
     },
     error: function(){
