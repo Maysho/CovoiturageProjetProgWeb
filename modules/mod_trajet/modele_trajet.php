2793cce2e8f4b542f922e933edb984de5a2ad459
William Lin <38981226+Maysho@users.noreply.github.com>
2019-01-15 12:22:38 +0100

    fix image

--- modules/mod_trajet/modele_trajet.php
+++ modules/mod_trajet/modele_trajet.php
@@ -50,7 +50,7 @@ class modele_trajet extends connexion {
 				$extension_upload = strtolower(  substr( strrchr($_FILES['photo']['name'], '.')  ,1)  );
 				$nomFich=$idConducteur.'_'.$immatriculation.'.'.$extension_upload;
 				// echo "FILE DEST = " . $_SERVER['DOCUMENT_ROOT']. "/CovoiturageProjetProgWeb/sources/images/photoVehicule/";
-				$result=move_uploaded_file($_FILES['photo']['tmp_name'],$_SERVER['DOCUMENT_ROOT']. "/CovoiturageProjetProgWeb/sources/images/photoVehicule/".$nomFich);
+				$result=move_uploaded_file($_FILES['photo']['tmp_name'],DIR . "/../../sources/images/photoVehicule/".$nomFich);
 				if($result)
 					$url = "sources/images/photoVehicule/".$nomFich;
 			}
